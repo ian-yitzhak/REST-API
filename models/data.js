a0f9c420aaec6b5539e4186b5b896d8c57209bff
Ian <onyangoian71@gmail.com>
2021-07-07 19:32:34 +0300

    Define post routes

---
+++ models/data.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const personSchema = new mongoose.Schema({
+	name :{
+
+		type: String,
+		required: true
+
+	},
+	email: {
+		type : String,
+		required: true
+	}
+})
+
+module.exports = mongoose.model('Person' , personSchema)
--- routes/data.js
+++ routes/data.js
@@ -2,8 +2,26 @@ const express = require('express')
 
 const router = express.Router()
 
+const Person = require('../models/data')
+
 router.get('/', (req,res)=>{
 	res.send('hello')
+})
+
+router.post('/', async (req,res)=>{
+
+	const person = new Person(req.body)
+
+	try{
+
+		await person.save()
+		res.status(201).send(person)
+	}
+	catch(err){
+
+		res.status(400).send(err)
+
+	}
 })
 
 module.exports = router
