4e5663fca6b4ff4ea2d536a1bc0a49e64b4fa234
Ian <onyangoian71@gmail.com>
2021-07-07 20:58:50 +0300

    Prepare deployment to github

--- db/db.js
+++ db/db.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(
-	"mongodb+srv://ian_test:METH71@sellah@ian.qfm7u.mongodb.net/auth?retryWrites=true&w=majority",
+	"mongodb+srv://ian_test:<Password>@db.qfm7u.mongodb.net/DATABSENAME?retryWrites=true&w=majority",
 	{ useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true  }
 
 	)
 .then(()=> console.log('Connected successfully'))
 .catch((err)=> console.log(err))
+
+//use your mongo db credentials
 
